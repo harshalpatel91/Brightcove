b3420407f9469dbdba989af32dfd8696c8b55a1a
harshalpatel <harshal.patel@raweng.com>
2018-06-29 15:26:05 +0530

    initial commit

--- app.js
+++ app.js
@@ -7,31 +7,27 @@ var request = require('request');
 var path = require('path');
 var multipart = require('connect-multiparty');
 
+// //Code to run if we're in the master process
+// if (cluster.isMaster) {
+//     // Count the machine's CPUs
+//     var cpuCount = os.cpus().length;
+//     // Create a worker for each CPU
+//     for (var i = 0; i < cpuCount; i++) {
+//         cluster.fork();
+//     }
 
-//self-signed certificates with Node
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-//Code to run if we're in the master process
-if (cluster.isMaster) {
-    // Count the machine's CPUs
-    var cpuCount = os.cpus().length;
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i++) {
-        cluster.fork();
-    }
-
-    // Listen for dying workers
-    cluster.on('exit', function (worker) {
-        console.log('Worker %d died', worker.id);
-        cluster.fork();
-    });
-} else {
+//     // Listen for dying workers
+//     cluster.on('exit', function (worker) {
+//         console.log('Worker %d died', worker.id);
+//         cluster.fork();
+//     });
+// } else {
     
-    var URL = "https://dev-api-contentstack.built.io";
+    var URL = "https://dev-new-api-contentstack.io";
 
     var port = process.env.PORT || 5000;
 
-    
+
 
     app.use('/', function(req, res) {
         req.pipe(request(URL + req.url)).pipe(res);
@@ -40,6 +36,5 @@ if (cluster.isMaster) {
     
     app.listen(port);
 
-   console.log('Worker[%d] : Application running on %d Port.', cluster.worker.id, port);
-}
+//}
 
