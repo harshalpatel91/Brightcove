89ccc270fbdfef6f780b4371ce34825546f58e75
harshalpatel <harshal.patel@raweng.com>
2018-06-29 15:29:33 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-var cluster = require('cluster');
 var express = require('express');
 var os = require('os');
 var fs = require('fs');
 var app = express();
 var request = require('request');
 var path = require('path');
-var multipart = require('connect-multiparty');
 
-// //Code to run if we're in the master process
-// if (cluster.isMaster) {
-//     // Count the machine's CPUs
-//     var cpuCount = os.cpus().length;
-//     // Create a worker for each CPU
-//     for (var i = 0; i < cpuCount; i++) {
-//         cluster.fork();
-//     }
 
-//     // Listen for dying workers
-//     cluster.on('exit', function (worker) {
-//         console.log('Worker %d died', worker.id);
-//         cluster.fork();
-//     });
-// } else {
-    
     var URL = "https://dev-new-api-contentstack.io";
 
     var port = process.env.PORT || 5000;
@@ -36,5 +19,4 @@ var multipart = require('connect-multiparty');
     
     app.listen(port);
 
-//}
 
