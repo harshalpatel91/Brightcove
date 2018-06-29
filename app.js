755ecdff3a6eef4294d0571fd0a0663250113cac
harshalpatel <harshal.patel@raweng.com>
2018-06-29 17:25:23 +0530

    different proxy for oauth and brightcove

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ var request = require('request');
 var path = require('path');
 
 
-var URL = "https://oauth.brightcove.com";
-
 var port = process.env.PORT || 5000;
 
 
+app.use('/brightcove', function(req, res) {
+	var brightcoveUrl = "https://cms.api.brightcove.com";
+	
+    req.pipe(request(brightcoveUrl + req.url)).pipe(res);
 
-app.use('/', function(req, res) {
-    req.pipe(request(URL + req.url)).pipe(res);
+});
+
+app.use('/oauth', function(req, res) {
+	var oauthUrl = "https://oauth.brightcove.com";
+	console.log("req",req, req.url);
+    req.pipe(request(oauthUrl + req.url)).pipe(res);
+
 });
 
 
